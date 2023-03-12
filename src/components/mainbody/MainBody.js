ba034d8356cfee49e6d21f3cb9c9c9210302bf40
sina mansoory <sinaman7979@gmail.com>
2023-03-12 22:27:15 +0330

    adding reason why section to main body.

--- src/components/mainbody/MainBody.js
+++ src/components/mainbody/MainBody.js
@@ -1,12 +1,16 @@
 import { Container } from "@mui/material";
 import React from "react";
-import Search from "./shared/Search";
+import SearchSection from "./shared/SearchSection";
+import WhyDrSaina from "./shared/WhyDrSaina";
+
+import styles from './MainBody.module.css';
 
 const MainBody = () => {
     return(
         <Container maxWidth='lg'>
-            <div>
-                <Search />
+            <div className={styles.container}>
+                <SearchSection />
+                <WhyDrSaina />
             </div>
         </Container>
        
